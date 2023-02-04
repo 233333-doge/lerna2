b16e8a79bc262687b121e3b7b2b451d5383cfc09
王帆 <9729143+wang-fan1111@user.noreply.gitee.com>
2023-02-04 17:46:35 +0800

    11

--- packages/init/command.js
+++ packages/init/command.js
@@ -1,7 +1,7 @@
 exports.command = "init";
 exports.describe = "创建一个新的lerna仓库";
 exports.builder = (yargs) => {
-  // console.log("init builder", yargs);
+  console.log("init builder", yargs);
 };
 // init 真正的处理逻辑
 exports.handler = (argv) => {
